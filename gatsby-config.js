c96a4886e6c357704ce247139caf2bf90063e262
paweltar <paweltar@gmail.com>
2017-10-12 10:56:13 +0200

    Add n-progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      }
+    }
+],
 }
