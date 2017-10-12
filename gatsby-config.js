70e2efc19b64ea44751e4c60ed823af3253982af
paweltar <paweltar@gmail.com>
2017-10-12 13:13:27 +0200

    Add offline support and manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,34 @@ module.exports = {
     title: `Gatsby Default Starter`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "minimal-ui",
+        icons: [
+          {
+            // Everything in /static will be copied to an equivalent
+            // directory in /public during development and build, so
+            // assuming your favicons are in /static/favicons,
+            // you can reference them here
+            src: `/favicons/favicon-196x196.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/mstile-310x310.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-nprogress`,
@@ -17,6 +45,25 @@ module.exports = {
       resolve: `gatsby-plugin-sass`,
       options: {
         precision: 8,
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              // Remove the default behavior of adding a link to each
+              // image.
+              linkImagesToOriginal: false,
+            },
+          },
+        ]
       }
     }
 ],
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,8 +3,33 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <article className="uk-article">
+    
+        <h1 className="uk-article-title"><a className="uk-link-reset" href="">Heading</a></h1>
+    
+        <p className="uk-article-meta">Written by <a href="#">Super User</a> on 12 April 2012. Posted in <a href="#">Blog</a></p>
+
+        <div className="uk-cover-container uk-height-medium">
+          <img src="https://picsum.photos/1000/950/?random" alt="" data-uk-cover/>
+        </div>
+    
+        <p className="uk-text-lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+    
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    
+        <div className="uk-grid-small uk-child-width-auto" data-uk-grid>
+            <div>
+                <a className="uk-button uk-button-text" href="#">Read more</a>
+            </div>
+            <div>
+                <a className="uk-button uk-button-text" href="#">5 Comments</a>
+            </div>
+            <div>
+              <span data-uk-icon="icon: heart"></span>
+            </div>
+        </div>
+    
+    </article>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
